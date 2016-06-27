7a4c1de7e8b5e8db4f6bfd7eb60107e44ed1166c
엄태현 <jeffeom@jeffs-macbook-pro.local>
2016-06-27 16:28:45 -0700

    Initial Commit

---
+++ stringManipulation/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  stringManipulation
+//
+//  Created by Jeff Eom on 2016-06-27.
+//  Copyright © 2016 Jeff Eom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
